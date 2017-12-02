c0c44980c628ae56e7f2983a89da5f0b00efb234
DragonflyStats <kobriendublin@gmail.com>
2017-12-02 13:39:27 +0000

    Create Correlation-Homeworks.R

---
+++ Correlation-Homeworks.R
@@ -0,0 +1,10 @@
+
+
+seed1 = sample(1000:1999,1)
+seed2 = sample(2000:2999,1)
+
+cat("\nCompute the Correlation Coefficient for the following data sets")
+
+
+cat("\n\nHint: To generate X, simply type in set.seed(",seed1,"); X <- sample(100:150,10,TRUE)")
+cat("\n\nHint: To generate Y, simply type in set.seed(",seed2,"); Y <- sample(100:150,10,TRUE)")
